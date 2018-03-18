222de34c57a9cc4b2a9047f3c7bff374f2b5d48e
John Evans <lgastako@gmail.com>
2018-03-18 02:57:48 -0600

    These changes

--- src/Dispenser/Server.hs
+++ src/Dispenser/Server.hs
@@ -13,13 +13,13 @@ import Dispenser.Server.Partition       as Exports ( pgConnect
 import Dispenser.Server.Primitives      as Exports ( pgAppendEvents
                                                    , pgPostEvent
                                                    )
-import Dispenser.Server.Streams.Batched as Exports ( rangeStream )
+import Dispenser.Server.Streams.Batched as Exports ( pgRangeStream )
 import Dispenser.Server.Streams.Catchup as Exports ( fromEventNumber
                                                    , fromZero
                                                    )
 import Dispenser.Server.Streams.Event   as Exports ( currentStream
                                                    , currentStreamFrom
                                                    )
-import Dispenser.Server.Streams.Push    as Exports ( fromNow )
+import Dispenser.Server.Streams.Push    as Exports ( pgFromNow )
 import Dispenser.Server.Types           as Exports
 
