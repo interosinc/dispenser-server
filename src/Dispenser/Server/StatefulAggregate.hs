5d7b1493aea0396743425d9ba0182df0a9cc300c
John Evans <lgastako@gmail.com>
2018-03-29 02:15:31 -0600

    Checkpoint to try stream instead of StreamFrom

--- src/Dispenser/Server/StatefulAggregate.hs
+++ src/Dispenser/Server/StatefulAggregate.hs
@@ -8,6 +8,7 @@ module Dispenser.Server.StatefulAggregate where
 
 import Dispenser.Prelude           hiding ( state )
 
+import qualified Streaming.Prelude as S
 import Control.Concurrent.STM.TVar
 import Dispenser.Types
 import Streaming
@@ -33,16 +34,40 @@ class AggregateSource source where
                -> m (Either AggregateError (StatefulAggregate m a x b s))
 
 data EphemeralAggregateSource m e r = EphemeralAggregateSource
-  { streamFrom :: MonadIO m => [StreamName] -> EventNumber -> m (Stream (Of (Event e)) m r)
+  { streamFrom :: MonadIO m
+               => [StreamName] -> EventNumber -> m (Stream (Of (Event e)) m r)
   }
 
-instance MonadIO m => AggregateSource (EphemeralAggregateSource m e r) where
-  findOrCreate :: forall m a x b s source. MonadIO m
+-- forall m a x b s. MonadIO m
+
+instance AggregateSource (EphemeralAggregateSource m e r) where
+  -- findOrCreate :: MonadIO m1 => AggregateConfig m1 a x b s
+  --              -> EphemeralAggregateSource m e r
+  --              -> m1 (Either AggregateError (StatefulAggregate m1 a x b s))
+  findOrCreate :: forall m a x b s. MonadIO m
                => AggregateConfig m a x b s
-               -> source
+               -> EphemeralAggregateSource m e r
                -> m (Either AggregateError (StatefulAggregate m a x b s))
-  findOrCreate config@AggregateConfig {..} _ =
-    Right . StatefulAggregate config <$> (liftIO . atomically . newTVar =<< initial)
+  findOrCreate config@AggregateConfig {..} EphemeralAggregateSource{..} = do
+    var <- (liftIO . atomically . newTVar =<< initial)
+    -- TODO: monitor the thread for crashing and crash the aggregate
+    -- void . liftIO . forkIO . forever $ do
+    --   undefined
+    stream <- streamFrom streamNames eventNumber'
+    -- void . liftIO . forkIO  $ do
+    --   S.effects $ S.mapM f stream
+    return . Right . StatefulAggregate config $ var
+    where
+      f = undefined
+
+      eventNumber' :: EventNumber
+      eventNumber' = undefined
+
+      -- TODO:
+      streamNames = []
+
+  -- findOrCreate config@AggregateConfig {..} _ =
+  --   Right . StatefulAggregate config <$> (liftIO . atomically . newTVar =<< initial)
 
 instantValue :: MonadIO m => StatefulAggregate m a x b s -> m b
 instantValue StatefulAggregate {..} = extract =<< (liftIO . atomically . readTVar $ state)
