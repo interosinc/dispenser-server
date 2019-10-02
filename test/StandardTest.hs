284e0bcb1964098da4fc5e3a63f833ed13b88be1
John Evans <lgastako@gmail.com>
2019-10-02 11:28:41 -0400

    Clean up StandardTest driver.

--- test/StandardTest.hs
+++ test/StandardTest.hs
@@ -17,25 +17,25 @@ import Dispenser.Server             ( PgClient
                                     )
 import Dispenser.Tests              ( TestConfig( TestConfig )
                                     , TestEvent
-                                    , trivialSpecFromClient
+                                    , partitionConnectionSpecFrom
                                     )
 import Test.Tasty.Hspec             ( Spec
                                     , runIO
                                     )
 
 spec_standard :: Spec
-spec_standard = do
-  let testCfg :: TestConfig (PgClient TestEvent) PgConnection
-      testCfg = TestConfig makeConn
-        where
-          makeConn :: ResourceT IO (PgConnection TestEvent)
-          makeConn = do
-            -- TODO: env vars
-            let poolMax = 5
-                url = "postgres://dispenser:dispenser@localhost:5432/dispenser"
-            client :: PgClient TestEvent <- liftIO $ new poolMax url
-            connect (PartitionName "randomize-me") client
+spec_standard = join . runIO . runResourceT $
+  partitionConnectionSpecFrom testCfg "PgClient"
+  where
+    testCfg :: TestConfig (PgClient TestEvent) PgConnection
+    testCfg = TestConfig makeConn
+      where
+        makeConn :: ResourceT IO (PgConnection TestEvent)
+        makeConn = do
+          -- TODO: env vars
+          let poolMax = 5
+              url = "postgres://dispenser:dispenser@localhost:5432/dispenser"
+          client :: PgClient TestEvent <- liftIO $ new poolMax url
+          connect (PartitionName "randomize-me") client
 
-  generatedSpec <- runIO . runResourceT $
-    trivialSpecFromClient testCfg "PgClient"
-  generatedSpec
+
